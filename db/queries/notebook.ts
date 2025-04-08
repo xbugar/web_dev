761a3a6111dc773924e5d9b7b62dd3c2e1929316
Andrej Bugar <xbugar@fi.muni.cz>
2025-04-08 14:41:07 +0200

    fix(databse): database model + big mock data upgrade

--- db/queries/notebook.ts
+++ db/queries/notebook.ts
@@ -1,20 +1,22 @@
 import { prisma } from '../client'
 
-export async function createNotebook(userId: string, iconId: string, title: string) {
+export async function createNotebook(userId: string, iconId: string, title: string, description: string, color: string) {
     return prisma.notebook.create({
         data: {
             title: title,
+            description: description,
             userId: userId,
             iconId: iconId,
+            color: color,
         }
     })
 }
 
-export async function createNote(notebookId: string, title: string, content: string) {
+export async function createNote(notebookId: string, title: string, color: string) {
     return prisma.note.create({
         data: {
             title: title,
-            content: content,
+            color: color,
             notebookId: notebookId,
         }
     })
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Icon, PrismaClient, ProfilePicture } from '@prisma/client'
+import {Icon, Importance, PrismaClient, ProfilePicture} from '@prisma/client'
 import { addProfilePicture } from "../db/queries/profilePicture";
 import { addUser, getUserCalendar } from "../db/queries/user";
 import { createEvent, addEventTime } from "../db/queries/calendar";
@@ -6,31 +6,32 @@ import { addIcon } from "../db/queries/icon";
 import {createNote, createNotebook} from "../db/queries/notebook";
 import assert from "node:assert";
 import {createFlashAnswer, createFlashCard, createFlashDeck} from "../db/queries/FlashDeck";
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient()
 
-async function createUserWithEvents(picture: ProfilePicture) {
-    const user = await addUser("Natalka", "", "example@mail.ls", "", "", picture.id);
+async function createUserWithEvents(name: string, mail: string, picture: ProfilePicture) {
+    const user = await addUser(name, "", mail, "", "", picture.id);
     const userCalendar = await getUserCalendar(user.id);
     assert(userCalendar);
-    const userEvent = await createEvent(userCalendar.id, "sumn", new Date(), new Date());
+    const userEvent = await createEvent(userCalendar.id, "", new Date(), new Date(), Importance.HIGH, "red", "");
     await addEventTime(userEvent.id, new Date(), new Date());
     return user;
 }
 
 
 
-async function createUserWithNotebook(picture: ProfilePicture, icon: Icon) {
-    const user = await addUser("Janka", "", "example@mail.cd", "", "", picture.id);
-    const userNotebook = await createNotebook(user.id, icon.id, "test Notebook");
-    await createNote(userNotebook.id, "test note", "very important message");
+async function createUserWithNotebook(name: string, mail: string, picture: ProfilePicture, icon: Icon) {
+    const user = await addUser(name, "", mail, "", "", picture.id);
+    const userNotebook = await createNotebook(user.id, icon.id, "test Notebook", "", "red");
+    await createNote(userNotebook.id, "test note", "red");
     return user;
 }
 
 
-async function createUserWithFlashDeck(picture: ProfilePicture, icon: Icon) {
-    const user = await addUser("Jozef", "", "example@mail.rm", "", "", picture.id);
-    const userFlashDeck = await createFlashDeck(user.id, icon.id, "test FlashDeck");
+async function createUserWithFlashDeck(name: string, mail: string, picture: ProfilePicture, icon: Icon) {
+    const user = await addUser(name, "", mail, "", "", picture.id);
+    const userFlashDeck = await createFlashDeck(user.id, icon.id, "test FlashDeck", "", "red");
     const userFlashCard = await createFlashCard(userFlashDeck.id, "Are traps gay?");
     const userFlashAnswer1 = await createFlashAnswer(userFlashCard.id, "yes", false);
     const userFlashAnswer2 = await createFlashAnswer(userFlashCard.id, "no", true);
@@ -43,11 +44,18 @@ async function main() {
     const icon = await addIcon("db/mockData/exclamation.svg");
 
 
-    createUserWithEvents(picture).catch(ex => console.error("something went wrong: createUserWithEvents\n", ex));
-    createUserWithNotebook(picture, icon).catch(ex => console.error("something went wrong: createUserWithNoteBook\n", ex));
-    createUserWithFlashDeck(picture, icon).catch(ex => console.error("something went wrong: createUserWithFlashDeck\n", ex));
+    createUserWithEvents("Natalka", "example@mail.ls",picture).catch(ex => console.error("something went wrong: createUserWithEvents\n", ex));
+    createUserWithNotebook("Janka", "example@mail.cd", picture, icon).catch(ex => console.error("something went wrong: createUserWithNoteBook\n", ex));
+    createUserWithFlashDeck("Jozef", "example@mail.rm", picture, icon).catch(ex => console.error("something went wrong: createUserWithFlashDeck\n", ex));
     addUser("Tomas", "", "example@mail.ps", "", "", picture.id).catch(ex => console.error(ex));
     addUser("andrej", "", "example@mail.sh", "", "", picture.id).catch(ex => console.error(ex));
+
+    for (let i = 0; i < 100; i++) {
+        createUserWithEvents(faker.person.firstName(), faker.internet.email(),picture).catch(ex => console.error("something went wrong: createUserWithEvents\n", ex));
+        createUserWithNotebook(faker.person.firstName(), faker.internet.email(), picture, icon).catch(ex => console.error("something went wrong: createUserWithNoteBook\n", ex));
+        createUserWithFlashDeck(faker.person.firstName(), faker.internet.email(), picture, icon).catch(ex => console.error("something went wrong: createUserWithFlashDeck\n", ex));
+    }
+
 
 }
 
