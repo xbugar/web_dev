37a5e31bb2a39ac9904c3fb6ca1151ae7d52f144
Tomas Bokor <xbokor@fi.muni.cz>
2025-04-12 16:54:58 +0200

    feat: added tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    tailwindcss(),
     react()
   ],
 })
