71d54b241e5a5d1013acbb216f55838aa56d3573
Natalia Ligacova <x539936@fi.muni.cz>
2025-04-17 01:47:37 +0200

    feat: add props to notebook card

--- frontend/src/components/cards/NotebookCard.tsx
+++ frontend/src/components/cards/NotebookCard.tsx
@@ -5,40 +5,51 @@ import {
   CardTitle,
 } from "@/components/ui/card.tsx";
 
-import {BookOpen, Notebook, Timer} from "lucide-react";
-import {NotebookNoteDropdown} from "@/components/NotebookNoteDropdown.tsx";
-import {Badge} from "@/components/ui/badge.tsx";
+import { LucideIcon, Notebook, Timer } from "lucide-react";
+import { NotebookNoteDropdown } from "@/components/NotebookNoteDropdown.tsx";
+import { Badge } from "@/components/ui/badge.tsx";
 
-export function NotebookCard() {
+type NotebookCardProps = {
+  title: string;
+  description: string;
+  Icon: LucideIcon;
+  // color: string; todo
+  noteCount: number;
+  lastUpdated: string;
+  tags: { name: string; color: "blue" | "purple" | "yellow" | "green" | "red" | "pink" }[];
+}
+
+export function NotebookCard({title, description, Icon, noteCount, lastUpdated, tags} : NotebookCardProps) {
   return (
       <Card className="flex border-l-10 border-l-accent-blue overflow-hidden p-0 pt-3 gap-3">
         <CardHeader className="flex pl-0 gap-2 items-center justify-start self-stretch">
           <div className="p-2.5 bg-accent-blue rounded-r-md">
-            <BookOpen className=" text-text-primary-white w-6 h-6"/>
+            <Icon className=" text-text-primary-white w-6 h-6"/>
           </div>
           <div className="flex-auto items-start">
             <div className="flex justify-between items-center self-stretch">
-              <CardTitle>Title of Notebook</CardTitle>
+              <CardTitle>{title}</CardTitle>
               <NotebookNoteDropdown/>
             </div>
             <div
                 className="text-text-lm-secondary dark:text-text-dm-secondary flex justify-between items-center self-stretch">
               <div className="flex justify-start items-center gap-1">
                 <Notebook className="w-4 h-4"/>
-                <CardDescription> 4 notes </CardDescription>
+                <CardDescription> {noteCount} notes </CardDescription>
               </div>
               <div className="flex justify-start items-center gap-1">
                 <Timer className="w-4 h-4"/>
-                <CardDescription> Updated 2 weeks ago</CardDescription>
+                <CardDescription> Updated {lastUpdated} ago </CardDescription>
               </div>
             </div>
           </div>
         </CardHeader>
         <div className="flex gap-2 justify-start self-stretch pl-3 pr-3">
-          <Badge variant="blue">Programming</Badge>
-          <Badge variant="purple">Semester 4</Badge>
+          {tags.map((tag, index) => (
+              <Badge key={index} variant={tag.color}> {tag.name} </Badge>
+          ))}
         </div>
-        <CardDescription className="pl-3 pr-3 pb-3">Deploy your new project in one-click.</CardDescription>
+        <CardDescription className="pl-3 pr-3 pb-3"> {description} </CardDescription>
       </Card>
   )
 }
