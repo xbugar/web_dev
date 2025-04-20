cbe6693a7568d9836f1d66c9099ad2a7fe26b679
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-04-20 23:39:48 +0200

    feat: create types and validation schema for user

---
+++ backend/apis/user/types.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+import {userCreateRequestSchema,userUpdateRequestSchema,userCreateNotebookRequestSchema} from "./validationSchemas"
+
+export type User = {
+    id:string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+}
+
+export type UserUpdateRequest = z.infer<typeof userUpdateRequestSchema>;
+
+export type UserCreateRequest = z.infer<typeof userCreateRequestSchema>;
+
+export type UserCreateNotebookRequest = z.infer<typeof userCreateNotebookRequestSchema>;
+
---
+++ backend/apis/user/validationSchemas.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+
+export const userCreateRequestSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    password: z.string().nonempty(),
+})
+
+export const userUpdateRequestSchema = z.object({
+        params: z.object({
+            userId: z.string().uuid()
+        }),
+        body: z.object({
+            firstName: z.string().nullable().optional(),
+            lastName: z.string().nullable().optional(),
+            email: z.string().email().nullable().optional(),
+            password: z.string().nonempty().nullable().optional(),
+        })
+    }
+)
+
+export const userCreateNotebookRequestSchema = z.object({
+    params: z.object({
+        userId: z.string().uuid()
+    }),
+
+    body: z.object({
+        title: z.string().min(1).max(255),
+        description: z.string().min(1).max(255),
+        color: z.string(),
+        iconId: z.string(),
+    })
+})
+
