d360dc3801ea0803efca6c6cb0c58c150a152221
Jana Kmoskova <xkmoskov@fi.muni.cz>
2025-04-24 19:21:03 +0200

    feat: public route

--- frontend/src/routes/_public/route.tsx
+++ frontend/src/routes/_public/route.tsx
@@ -7,7 +7,18 @@ export const Route = createFileRoute('/_public')({
 function RouteComponent() {
   return (
     <>
-      <Outlet />
+        <div className="flex p-2 flex-col items-start gap-2.5">
+            <div className="flex items-center p-4 self-stretch justify-between">
+                <div className="justify-center text-white text-3xl font-bold font-serif leading-loose">gradia.</div>
+                <div className="w-28 text-center justify-center text-Primary-Text-White text-xs font-serif">Write it down, make it happen.</div>
+            </div>
+
+            <div className="self-stretch p-4 inline-flex justify-between items-center">
+                <div>
+                    <Outlet />
+                </div>
+            </div>
+        </div>
     </>
   )
 }
