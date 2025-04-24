7b3546fd9ab67e6fb55062994f9e4cf38404dc34
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-04-24 17:18:37 +0200

    feat: add some wip tests

--- backend/test.index.ts
+++ backend/test.index.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 import cors from 'cors';
 import {userRouter} from "./apis/user/router";
+import {notebookRouter} from "./apis/notebook/router";
+import {notesRouter} from "./apis/note/router";
+import {tagsRouter} from "./apis/tag/router";
 
 
 const app = express();
@@ -13,5 +16,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter);
+app.use('/notebook', notebookRouter);
+app.use('/note',notesRouter);
+app.use('/tag', tagsRouter);
 
 export default app;
---
+++ backend/tests/notebook.test.ts
@@ -0,0 +1,5 @@
+import { describe, expect, it } from 'vitest'
+import request from 'supertest'
+import app from '../test.index'
+import {prisma} from "./utils/prisma";
+
