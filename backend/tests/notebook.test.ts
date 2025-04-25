46ad8a57d246809e4f41897e25bddcb65a65bb1b
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-04-25 10:28:44 +0200

    fix: add empty tests

--- backend/tests/notebook.test.ts
+++ backend/tests/notebook.test.ts
@@ -3,3 +3,8 @@ import request from 'supertest'
 import app from '../test.index'
 import {prisma} from "./utils/prisma";
 
+describe("/notebook", async () => {
+    describe("happy path", async () => {
+        it(`should do nothing`, async () => {})
+    })
+})
