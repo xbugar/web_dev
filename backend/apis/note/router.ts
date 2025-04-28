d70b636859e99130684adbf32267d26ca68cdfc4
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-04-29 00:14:38 +0200

    fix: typo in route

--- backend/apis/note/router.ts
+++ backend/apis/note/router.ts
@@ -6,8 +6,8 @@ export const notesRouter = Router();
 notesRouter.get("/:noteId", notesController.getNoteMeta);
 notesRouter.put("/:noteId", notesController.updateNoteMeta);
 notesRouter.delete("/:noteId", notesController.deleteNote);
-notesRouter.get("/:noteId/Content", notesController.getNoteContent);
-notesRouter.put("/:noteId/Content", notesController.updateNoteContent);
+notesRouter.get("/:noteId/content", notesController.getNoteContent);
+notesRouter.put("/:noteId/content", notesController.updateNoteContent);
 notesRouter.post("/:noteId/tag/:tagId", notesController.addNoteTag);
 notesRouter.delete("/:noteId/tag/:tagId", notesController.removeNoteTag);
 
