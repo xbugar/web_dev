a0ec3a7467442a6b64d3ed124c5e678ac1436aee
Tomas Bokor <xbokor@fi.muni.cz>
2025-04-29 13:02:07 +0200

    ...

--- frontend/src/routes/_authentificated/route.tsx
+++ frontend/src/routes/_authentificated/route.tsx
@@ -1,15 +1,15 @@
-import { Header } from '@/components/header/Header'
-import { Navigation } from '@/components/navigation/Navigation'
-import { createFileRoute, Outlet, useMatchRoute } from '@tanstack/react-router'
+import { Header } from '@/components/header/Header';
+import { Navigation } from '@/components/navigation/Navigation';
+import { createFileRoute, Outlet, useMatchRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authentificated')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const matchRoute = useMatchRoute()
-  const params = matchRoute({ to: '/notebooks/$notebookId/$noteId' })
-  console.log(params)
+  const matchRoute = useMatchRoute();
+  const params = matchRoute({ to: '/notebooks/$notebookId/$noteId' });
+  console.log(params);
   return (
     <>
       <Header />
