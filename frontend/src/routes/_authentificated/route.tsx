9c6969e766a7cea95fd900bfd149bf0a1a9df91c
Jana Kmoskova <xkmoskov@fi.muni.cz>
2025-04-29 12:20:27 +0200

    fix: deleted nav bar in note page

--- frontend/src/routes/_authentificated/route.tsx
+++ frontend/src/routes/_authentificated/route.tsx
@@ -1,19 +1,22 @@
 import { Header } from '@/components/header/Header'
 import { Navigation } from '@/components/navigation/Navigation'
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, useMatchRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authentificated')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const matchRoute = useMatchRoute()
+  const params = matchRoute({ to: '/notebooks/$notebookId/$noteId' })
+  console.log(params)
   return (
     <>
       <Header />
       <section className='px-4 mb-20'>
         <Outlet />
       </section>
-      <Navigation />
+      {!params && <Navigation />}
     </>
   )
 }
