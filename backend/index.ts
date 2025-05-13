5ea9fa0368438a74f81395cdac4df5079f1c9712
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-05-14 00:56:56 +0200

    feat: add google cloud link and change when are docs shown

--- backend/index.ts
+++ backend/index.ts
@@ -52,7 +52,7 @@ app.use('/tag', passport.session(), isAuthenticated, tagsRouter);
 const swaggerYaml = fs.readFileSync("./api-documentation/openapi.yaml", "utf8");
 const swaggerDocument = yaml.parse(swaggerYaml);
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "dev-with-docs") {
     app.use(
         "/api-documentation",
         swaggerUi.serve,
