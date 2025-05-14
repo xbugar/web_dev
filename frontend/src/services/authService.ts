2085f441b318bf283c03ecc8105082dc1e7234f9
Natalia Ligacova <x539936@fi.muni.cz>
2025-05-14 22:46:41 +0200

    fix: login

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,10 +1,10 @@
 import { api } from '@/services/index.ts';
-import type { CreateUser, LoginUser, SessionId } from '@/types/User.ts';
+import type { CreateUser, LoginUser } from '@/types/User.ts';
 
 export const postRegisterUser = async (user: CreateUser) => {
   return api.post(`/auth/register`, user);
 };
 
 export const postLoginUser = async (user: LoginUser) => {
-  return api.post(`/auth/login`, user).then(response => response.data as SessionId);
+  return api.post(`/auth/login`, user);
 }
