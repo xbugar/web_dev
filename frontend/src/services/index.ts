791b3ac940943fa63bcb8bbd0a0c70ccda45c399
Natalia Ligacova <x539936@fi.muni.cz>
2025-05-14 19:15:00 +0200

    fix: server url

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = 'https://backend-511443014815.europe-west1.run.app';
+const SERVER_URL = 'https://backend-511443014815.europe-west1.run.app/';
 
 export const api = axios.create({
   baseURL: SERVER_URL,
