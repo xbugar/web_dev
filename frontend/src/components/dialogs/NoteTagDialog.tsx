7d5b1e5dfcf33d61655fafc7012731c003085081
Natalia Ligacova <x539936@fi.muni.cz>
2025-05-15 15:34:36 +0200

    fix: tag preview container

--- frontend/src/components/dialogs/NoteTagDialog.tsx
+++ frontend/src/components/dialogs/NoteTagDialog.tsx
@@ -44,23 +44,25 @@ export function NoteTagDialog({ open, onOpenChange, noteId, notebookId }: NoteTa
         <DialogHeader>
           <DialogTitle>Edit tags</DialogTitle>
           <DialogDescription></DialogDescription>
-          {noteData && noteData.tags && noteData.tags.length > 0 ? (
-            <div className="relative mr-4">
-              <div className="hide-scrollbar relative flex gap-2 overflow-x-auto pl-4">
-                {noteData.tags.map((tag, index) => (
-                  <Tag name={tag.name} color={tag.color as TagColor} key={index}></Tag>
-                ))}
-                <div className="ml-5"></div>
-              </div>
-
-              {/* shadow on the left side */}
-              {/* <div className="pointer-events-none absolute top-0 left-0 h-full w-3 bg-gradient-to-r from-white-secondary dark:from-black-secondary to-transparent"></div> */}
-              <div className="from-white-secondary dark:from-black-secondary pointer-events-none absolute top-0 right-0 h-full w-4 bg-gradient-to-l to-transparent"></div>
-            </div>
-          ) : (
-            <p className="text-sm text-muted-foreground italic">No tags</p>
-          )}
         </DialogHeader>
+        {noteData && noteData.tags && noteData.tags.length > 0 ? (
+          <div className="hide-scrollbar max-h-24 overflow-y-auto mb-4">
+            <div className="flex flex-wrap gap-2 justify-center">
+              {noteData.tags.map((tag, index) => (
+                <Tag
+                  name={tag.name}
+                  color={tag.color as TagColor}
+                  key={index}
+                />
+              ))}
+            </div>
+            {/* shadow on the left side */}
+            {/* <div className="pointer-events-none absolute top-0 left-0 h-full w-3 bg-gradient-to-r from-white-secondary dark:from-black-secondary to-transparent"></div> */}
+            <div className="from-white-secondary dark:from-black-secondary pointer-events-none absolute top-0 right-0 h-full w-4 bg-gradient-to-l to-transparent"></div>
+          </div>
+        ) : (
+          <p className="text-sm text-muted-foreground italic mb-4">No tags</p>
+        )}
         <TagForm
           onSubmit={handleCreateAndAssignTag}
           isSubmitting={createTag.isPending}
