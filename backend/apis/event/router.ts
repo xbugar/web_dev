08ade35db1bca1f5a0da27d4a5549ae69c44a256
Jozef Hoschek <xhoschek@fi.muni.cz>
2025-06-15 22:29:52 +0200

    fix: change router uri order

--- backend/apis/event/router.ts
+++ backend/apis/event/router.ts
@@ -7,8 +7,8 @@ eventRouter.get("", eventController.get);
 eventRouter.post("", eventController.createEvent);
 eventRouter.get("/date", eventController.getByDate);
 eventRouter.delete("/:eventId", eventController.deleteEvent);
+eventRouter.get("/range",eventController.getRange);//the uri is poorly named but honestly idc
 eventRouter.put("/:eventId", eventController.update);
 eventRouter.get("/:eventId", eventController.getById);
 eventRouter.post("/:eventId/tag", eventController.addTag);
 eventRouter.delete("/:eventId/tag/:tagId", eventController.removeTag);
-eventRouter.get("/range",eventController.getRange);//the uri is poorly named but honestly idc
