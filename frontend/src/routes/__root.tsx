996a26801332acd765ec507249b91d57d3e40029
Natalia Ligacova <x539936@fi.muni.cz>
2025-06-16 01:03:08 +0200

    feat: issue #24 solved

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { Toaster } from "sonner";
 // import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -6,7 +7,14 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          classNames: {
+            toast: 'w-fit px-4 py-2 rounded bg-black text-white text-sm',
+          },
+        }}
+      />      {/* <TanStackRouterDevtools /> */}
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </>
   ),
