64ab26f4d40b7df5c293f032baadb1c99de662ee
Natalia Ligacova <x539936@fi.muni.cz>
2025-06-17 01:51:59 +0200

    feat: podpisik

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -121,7 +121,7 @@ function RouteComponent() {
           </Link>
           <div className="flex flex-col items-center lg:items-end">
             <p>Created with ❤️ and code (obviously).</p>
-            <p>AB, JH, Jana Kmošková, NL, Tomáš Bokor</p>
+            <p>AB, JH, Jana Kmošková, Natália Ligačová, Tomáš Bokor</p>
             <p>MUNI FI PB138 2025</p>
           </div>
         </div>
