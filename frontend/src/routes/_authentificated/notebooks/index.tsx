353cfba482467619bcbeac60e88cf984e20ea1c1
Natalia Ligacova <x539936@fi.muni.cz>
2025-06-16 19:48:18 +0200

    fix: remove grid on desktop notebooks page

--- frontend/src/routes/_authentificated/notebooks/index.tsx
+++ frontend/src/routes/_authentificated/notebooks/index.tsx
@@ -28,9 +28,8 @@ function RouteComponent() {
   return (
     <div className="lg:h-[calc(100vh-1rem)] lg:overflow-hidden">
       <NotebookSection isPreview={false} />
-
       <div
-        className="grid grid-cols-1 gap-4 lg:h-[calc(100vh-5rem)] lg:auto-rows-max lg:grid-cols-2 lg:overflow-y-auto"
+        className="columns-1 gap-2 lg:columns-2 lg:h-[calc(100vh-5rem)] lg:overflow-y-auto"
         style={{
           scrollbarWidth: 'none',
           msOverflowStyle: 'none',
@@ -40,17 +39,19 @@ function RouteComponent() {
           notebooks
             .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
             .map(({ id, title, description, iconName, color, noteCount, tags, updatedAt }) => (
-              <NotebookCard
-                key={id}
-                id={id}
-                title={title}
-                description={description}
-                iconName={iconName}
-                color={color}
-                noteCount={noteCount}
-                tags={tags}
-                lastUpdated={updatedAt}
-              />
+              <div key={id} className="break-inside-avoid mb-4 px-1">
+                <NotebookCard
+                  key={id}
+                  id={id}
+                  title={title}
+                  description={description}
+                  iconName={iconName}
+                  color={color}
+                  noteCount={noteCount}
+                  tags={tags}
+                  lastUpdated={updatedAt}
+                />
+              </div>
             ))
         ) : (
           <EmptyState title={'No notebooks'} message={'Create a new notebook.'} />
