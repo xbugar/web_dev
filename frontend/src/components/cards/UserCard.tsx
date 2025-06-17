a321915f096fafa7640a1cf58070cb36047a284a
Natalia Ligacova <x539936@fi.muni.cz>
2025-06-17 03:01:17 +0200

    fix: black line

--- frontend/src/components/cards/UserCard.tsx
+++ frontend/src/components/cards/UserCard.tsx
@@ -1,48 +1,42 @@
 import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 
-import { AccentColor, lineColor } from '@/components/cards/cardColors';
 import { cn } from '@/lib/utils.ts';
-import {UserCardProps} from "@/types/AdminDashboard.ts";
-import {AdminUserDropdown} from "@/components/dropdown/AdminUserDropdown.tsx";
+import { UserCardProps } from "@/types/AdminDashboard.ts";
+import { AdminUserDropdown } from "@/components/dropdown/AdminUserDropdown.tsx";
 
 export function UserCard({
-                             userId,
-                             title,
-                             titleOfParent,
-                             color,
-                             }: UserCardProps) {
+                           userId,
+                           title,
+                           titleOfParent,
+                         }: UserCardProps) {
 
-    return (
-        <Card
-            className={cn(
-                'flex gap-4 overflow-hidden border-l-10 p-0 py-4 flex-shrink-0',
-                lineColor[color as AccentColor],
+  return (
+    <Card
+      className={cn(
+        'flex gap-4 overflow-hidden border-l-10 p-0 py-4 flex-shrink-0 border-l-black dark:border-l-white',
+      )}
+    >
+      <CardHeader className="flex items-center justify-start gap-2 px-4">
+        <div className="flex w-full flex-col gap-1">
+          <div className="flex w-full items-center justify-between">
+            <CardTitle>
+              {title}
+            </CardTitle>
+            {(
+              <AdminUserDropdown
+                userId={userId}
+              />
             )}
-        >
-            <CardHeader className="flex items-center justify-start gap-2 px-4">
-                <div className="flex w-full flex-col gap-1">
-                    <div className="flex w-full items-center justify-between">
-                        <CardTitle>
-                            {
-                            title
-                            }
-                        </CardTitle>
-                        { (
-                            <AdminUserDropdown
-                                userId={userId}
-                            />
-                        )}
-                    </div>
+          </div>
 
-                    <div className="text-black-text-secondary dark:text-white-text-secondary flex items-center justify-between self-stretch">
-                        <div className="flex gap-1">
-                            <CardDescription>{titleOfParent}</CardDescription>
-                        </div>
-
-                    </div>
-                </div>
-            </CardHeader>
-
-        </Card>
-    );
+          <div
+            className="text-black-text-secondary dark:text-white-text-secondary flex items-center justify-between self-stretch">
+            <div className="flex gap-1">
+              <CardDescription>{titleOfParent}</CardDescription>
+            </div>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
 }
--- frontend/src/routes/_authentificated/admin/index.tsx
+++ frontend/src/routes/_authentificated/admin/index.tsx
@@ -27,7 +27,7 @@ function RouteComponent() {
               userId={id}
               title={`${firstName} ${lastName} | ${email}`}
               titleOfParent={`Notebooks: ${notebookCount} Notes: ${totalNotes} Events: ${eventCount} Decks: ${deckCount} Cards: ${totalCards}`}
-              color={"blue"}
+              color={"black"}
             />
           ))}
       </div>
